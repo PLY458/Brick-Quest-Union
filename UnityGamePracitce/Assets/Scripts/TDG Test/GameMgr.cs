2bc1aa7094071957ead5c37d74fb3c1773608a45
20171308046 <monastars458@qq.com>
2020-11-18 12:08:10 +0800

    寻路版创建完成

---
+++ UnityGamePracitce/Assets/Scripts/TDG Test/GameMgr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TDG_game
+{
+        public class GameMgr : MonoBehaviour
+    {
+        [SerializeField]
+        Vector2Int boardSize = new Vector2Int(11, 11);
+
+        [SerializeField]
+        GameBoard board = default;
+
+        void Awake()
+        {
+            board.Initialize(boardSize);
+        }
+
+        void OnValidate()
+        {
+            if (boardSize.x < 2)
+            {
+                boardSize.x = 2;
+            }
+            if (boardSize.y < 2)
+            {
+                boardSize.y = 2;
+            }
+        }
+    }
+
+
+}
